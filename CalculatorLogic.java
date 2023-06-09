95a45ffe906ba7e398972498a3ae3638a67dbc21
kishore-086 <131332496+kishore-086@users.noreply.github.com>
2023-06-09 18:39:22 +0530

    day9

---
+++ CalculatorLogic.java
@@ -0,0 +1,80 @@
+package com.tasks.day11.day9;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CalculatorLogic {
+	
+	calculator cl =new calculator();
+	
+  @Test(dataProvider = "testData")
+  public void addMethod(int a,int b,int expected) {
+	  
+	  int result =cl.add(a,b);
+	  Assert.assertEquals(expected, result);
+	  
+  }
+  
+  @DataProvider
+  public Object [][] testData()
+  {
+	  return new Object [][]
+			  {
+		  {1,2,3}
+		  
+			  };
+  }
+  @Test(dataProvider = "testData1")
+  public void subMethod(int a,int b,int expected) {
+	  
+	  int result =cl.sub(a,b);
+	  Assert.assertEquals(expected, result);
+	  
+  }
+  
+  @DataProvider
+  public Object [][] testData1()
+  {
+	  return new Object [][]
+			  {
+		  {2,1,1}
+		  
+			  };
+  }
+  @Test(dataProvider = "testData2")
+  public void mulMethod(int a,int b,int expected) {
+	  
+	  int result =cl.mul(a,b);
+	  Assert.assertEquals(expected, result);
+	  
+  }
+  
+  @DataProvider
+  public Object [][] testData2()
+  {
+	  return new Object [][]
+			  {
+		  {1,2,2}
+		  
+			  };
+  }
+  @Test(dataProvider = "testData3")
+  public void divMethod(int a,int b,int expected) {
+	  
+	  int result =cl.div(a,b);
+	  Assert.assertEquals(expected, result);
+	  
+  }
+  
+  @DataProvider
+  public Object [][] testData3()
+  {
+	  return new Object [][]
+			  {
+		  {2,1,2}
+		  
+			  };
+  }
+  
+}
---
+++ calculator.java
@@ -0,0 +1,23 @@
+package com.tasks.day11.day9;
+
+public class calculator {
+	
+		public int add (int a, int b)
+		{
+			return a+b;
+		}
+	
+		public int sub (int a, int b)
+		{
+			return a-b;
+		}
+		public int mul (int a, int b)
+		{
+			return a*b;
+		}
+		public int div (int a, int b)
+		{
+			return a/b;
+		}
+
+}
